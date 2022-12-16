e4a2b73af74dc4a39fe478f83144e30c94eef4db
Guan-Xin <alex0972112871@gmail.com>
2022-12-16 17:56:45 +0800

    func: share styles; install primeng and primeflex

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+import { UiModule } from '@ngshop/ui';
+import { AccordionModule } from 'primeng/accordion';
+
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AccordionModule, UiModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
